1691be5e53c307ec627f4b9f1cc05b8123f5fd4e
Varga Kornél 9.B <93580730+Kornel0622@users.noreply.github.com>
2022-11-15 11:03:47 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+from genericpath import exists
+from os import system
+
+def kerdes1():
+    choice = ''
+    system('cls')
+    print()
+    print('1 - ')
+    print('2 - ')
+    print('3 - ')
+    choice = input('Válasz: ')
+    return choice
+
---
+++ main.py
@@ -0,0 +1,43 @@
+from functions import *
+
+kerdes1()
+choice = ''
+while choice != '0':
+    choice = kerdesMenu1()
+    if choice == '1':
+        valasz1()
+        input()
+    elif choice == '2':
+        valasz2()
+        input()
+    elif choice == '3':
+        valasz3()
+        input()
+
+kerdes2()
+choice = ''
+while choice != '0':
+    choice = kerdesMenu2()
+    if choice == '1':
+        valasz1()
+        input()
+    elif choice == '2':
+        valasz2()
+        input()
+    elif choice == '3':
+        valasz3()
+        input()
+
+kerdes3()
+choice = ''
+while choice != '0':
+    choice = kerdesMenu3()
+    if choice == '1':
+        valasz1()
+        input()
+    elif choice == '2':
+        valasz2()
+        input()
+    elif choice == '3':
+        valasz3()
+        input()
