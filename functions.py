b7900a5fc118ac78f1c56676b47e9cda322db027
Varga Kornél 9.B <93580730+Kornel0622@users.noreply.github.com>
2022-11-15 11:29:58 +0100

    kérdés1

--- functions.py
+++ functions.py
@@ -4,10 +4,10 @@ from os import system
 def kerdes1():
     choice = ''
     system('cls')
-    print()
-    print('1 - ')
-    print('2 - ')
-    print('3 - ')
+    print('Mikor született Hitler?')
+    print('1 - 1889 április 20.')
+    print('2 - 1903 január 10.')
+    print('3 - 1870 november 5.')
     choice = input('Válasz: ')
     return choice
 
