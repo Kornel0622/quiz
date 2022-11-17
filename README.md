51984b71f8e07afd1f49e3b7f39e983396e157a4
Varga Kornél 9.B <93580730+Kornel0622@users.noreply.github.com>
2022-11-17 12:51:41 +0100

    sok változás

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Quiz játék
 
 main.py-ba futtatható
 
-feltesz kérdéseket a program, ad 3 válasz lehetőséget és ki kell talalni, hogy melyik a jó
+feltesz kérdéseket a program, ha jól válaszol a felhasználó, akkor a program kiirja hogy "A válasz helyes!" és tovább visz a következő kérdésre, ha rosszul, akkor a program elmondja a helyes választ és továbbvisz a következő kérdésre
--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 from functions import *
 
-kerdes1()
-choice = ''
-while choice != '0':
-    choice = kerdesMenu1()
-    if choice == '1':
-        valasz1()
-        input()
-    elif choice == '2':
-        valasz2()
-        input()
-    elif choice == '3':
-        valasz3()
-        input()
 
-kerdes2()
-choice = ''
-while choice != '0':
-    choice = kerdesMenu2()
-    if choice == '1':
-        valasz1()
-        input()
-    elif choice == '2':
-        valasz2()
-        input()
-    elif choice == '3':
-        valasz3()
-        input()
+valasz = input("Melyik évben született Hitler? ")
+if valasz == "1889":
+    print("A válasz helyes!")
+else:
+    print(f"A válasz '1889', nem {valasz!r}")
 
 kerdes3()
 choice = ''
