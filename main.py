f85c9d5fdae7cb73d009aa5ecf08b4131f62499c
Varga Kornél 9.B <93580730+Kornel0622@users.noreply.github.com>
2022-11-22 09:47:02 +0100

    kisebb változtatások

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ kerdesek = [QA("Hány éves Orbán Viktor?", "59", ["70", "49", "60"]),
 QA("Ki a magyarországi meleg szeretője Orbán Viktornak?", "Gyurcsány Ferenc", ["Soros György", "Mészáros Lőrinc", "Németh Szilárd"]),
 QA("Mennyi ideje van hatalmon Orbán Viktor?", "Több ideje, mint kellett volna", ["12 év", "8 év", "20 év"]),
 QA("Milyen betegségben szenved Orbán Viktor?", "Elhízás", ["Rák", "Éc", "Down-szindróma"]),
-QA("Kik Orbán Viktor legnagyobb ellenségei?", "Gyurcsány", ["Transznemű óvodások", "Brüsszel", "Soros", ]),
+QA("Ki/Kik Orbán Viktor legnagyobb ellensége(i)?", "Gyurcsány", ["Transznemű óvodások", "Brüsszel", "Soros", ]),
 QA("Melyik állat hasonlít a legjobban Orbán Viktorra?", "Disznó", ["Bika", "Oroszlán", "Medve",])]
